c14b5ee917c51ac6f8acfffc2a2711140a53e180
mjsong07 <182254440@qq.com>
2024-07-25 10:44:42 +0800

    perf: 优化代码

--- vite/plugins/syncToPhp.ts
+++ vite/plugins/syncToPhp.ts
@@ -1,56 +1,64 @@
-import { Plugin } from 'vite'; 
-import fs from 'fs'; 
-import path from 'path';   
-import syncToPhpList from './syncToPhpList.json'
+import { Plugin } from 'vite';
+import fs from 'fs';
+import path from 'path';
+import syncToPhpList from './syncToPhpList.json';
 
-export default function syncToPhp(): Plugin {  
+// 确保目录存在的函数
+const ensureDirectoryExistence = (filePath:string) => {
+  const dirname = path.dirname(filePath);
+  if (fs.existsSync(dirname)) {
+    return true;
+  }
+  ensureDirectoryExistence(dirname);
+  fs.mkdirSync(dirname);
+};
+
+// 复制 HTML 文件的函数
+const copyHtmlFiles = (sourceHtml:string, targetBaseHtmlDir:string, htmlContent:string) => {
+  syncToPhpList.forEach(o => {
+    const newHtmlContent = `<script>window.__vue__router_path = '${o.name}'</script>\n${htmlContent}`;
+    const targetHtmlPath = path.join(targetBaseHtmlDir, o.path);
+    ensureDirectoryExistence(targetHtmlPath);
+    fs.writeFileSync(targetHtmlPath, newHtmlContent);
+  });
+};
+
+// 复制 Assets 文件的函数
+const copyAssetFiles = (sourceAssetDir:string, targetAssetDir:string) => {
+  const files = fs.readdirSync(sourceAssetDir);
+  files.forEach(file => {
+    const sourcePath = path.join(sourceAssetDir, file);
+    const targetPath = path.join(targetAssetDir, file);
+    ensureDirectoryExistence(targetPath);
+    fs.copyFileSync(sourcePath, targetPath);
+  });
+};
+
+export default function syncToPhp(): Plugin {
   return {
     name: 'syncToPhp',
-    apply: 'build', // 仅在构建时应用此插件 
+    apply: 'build', // 仅在构建时应用此插件
     closeBundle: async () => {
-        console.log('同步代码到php项目-->>开始'); 
-        const sourceHtml = './dist/index.html';  
-        const targetBaseHtmlDir = './../application/views';  
-        const sourceAssetDir = './dist/assets';
-        const targetAssetDir = '../public/vite/assets';  
+      console.log('同步代码到php项目-->>开始');
+      const sourceHtml = './dist/index.html';
+      const targetBaseHtmlDir = './../application/views';
+      const sourceAssetDir = './dist/assets';
+      const targetAssetDir = '../public/vite/assets';
 
-        if(syncToPhpList.length === 0) { 
-         console.error('构建的列表不能为空')
-         return
-        }
+      if (syncToPhpList.length === 0) {
+        console.error('构建的列表不能为空');
+        return;
+      }
 
-        const ensureDirectoryExistence = (filePath) => {
-          console.log("filePath",filePath)
-          const dirname = path.dirname(filePath);
-          if (fs.existsSync(dirname)) {
-            return true;
-          }
-          ensureDirectoryExistence(dirname);
-          fs.mkdirSync(dirname);
-        }; 
-        //复制 html 
-        try {
-          const htmlContent = fs.readFileSync(sourceHtml, 'utf-8');
-          syncToPhpList.forEach(async o => { 
-            const newHtmlContent = `<script>window.__vue__router_path = '${o.name}'</script>\n${htmlContent}`; 
-            let path = `${targetBaseHtmlDir}${o.path}` 
-            ensureDirectoryExistence(path); 
-            fs.writeFileSync(path, newHtmlContent);  
-          })  
-          //复制 assets 
-           let files = fs.readdirSync(sourceAssetDir); 
-            files.forEach(async file => {
-              let sourcePath = path.join(sourceAssetDir, file);
-              let targetPath = path.join(targetAssetDir, file);  
+      try {
+        const htmlContent = fs.readFileSync(sourceHtml, 'utf-8');
+        copyHtmlFiles(sourceHtml, targetBaseHtmlDir, htmlContent);
+        copyAssetFiles(sourceAssetDir, targetAssetDir);
+      } catch (error) {
+        console.error('构建异常', error);
+      }
 
-          ensureDirectoryExistence(targetPath); 
-                await fs.copyFileSync(sourcePath, targetPath);  
-            }); 
-        } catch (error) {
-          console.error('构建异常',error)
-        }
-      
-        console.log('同步代码到php项目-->>结束');
-    }  
+      console.log('同步代码到php项目-->>结束');
+    }
   };
 }
